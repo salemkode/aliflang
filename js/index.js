c3f0dea7c4f4565f55df981aa65df9ecf0ae011b
سالم محمد شماخ <58360393+superastorh@users.noreply.github.com>
2021-07-06 15:09:32 +0300

    nav fixed

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
+let $ = (e) => document.querySelector(e);
 class write {
   constructor(array = ["test"], node, time = 500) {
     this.node = node;
     let index = 0;
     let { all } = this;
     all = all.bind(this);
-    (async e => {
+    (async (e) => {
       while (true) {
         await all(array[index], time);
         index++;
@@ -14,8 +15,8 @@ class write {
   }
   write(string = "", time = 500) {
     let index = 0;
-    return new Promise(resolve => {
-      let loop = setInterval(e => {
+    return new Promise((resolve) => {
+      let loop = setInterval((e) => {
         this.node.innerHTML = string.slice(0, index);
         if (index === string.length) {
           clearInterval(loop);
@@ -28,8 +29,8 @@ class write {
   remove(time = 500) {
     let string = this.node.innerHTML;
     let index = string.length;
-    return new Promise(resolve => {
-      let loop = setInterval(e => {
+    return new Promise((resolve) => {
+      let loop = setInterval((e) => {
         this.node.innerHTML = string.slice(0, index);
         if (index === 0) {
           clearInterval(loop);
@@ -52,30 +53,31 @@ class write {
 let words = [
   "لغة مفتوحة المصدر",
   " تدعم البايثون، وسي بلاس بلاس",
-  "متعددة المنصات"
+  "متعددة المنصات",
 ];
-let subtitile = new write(words, document.querySelector("#subHead"), 300);
+let subtitile = new write(words, $("#subHead"), 300);
 function wait(milliseconds) {
-  return new Promise(resolve => setTimeout(resolve, milliseconds));
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
 
-document.querySelector("#date").innerHTML = new Date().getFullYear();
-document.querySelector("#down").onclick = function(){
-  let to = document.body.offsetHeight - document.querySelector("nav").offsetHeight;
-  window.scrollTo(0,to);
-}
-let style = document.querySelector(".phones").style;
-let runMore = false;
-style.height = "0px"
-document.querySelector("#more").onclick = async function(){
-  if(runMore) return; 
-  runMore =  !runMore;
-  if(style.height === "0px"){
-    style.height = "261.8px"
-    await wait(1000);
-  }else {
-    style.height = "0px"
-    await wait(1000);
+$("#date").innerHTML = new Date().getFullYear();
+$("#down").onclick = function () {
+  let to = document.body.offsetHeight - $("nav").offsetHeight;
+  window.scrollTo(0, to);
+};
+let navHeight;
+document.body.onload = function () {
+  navHeight = $("ul").offsetHeight + "px";
+  $("#menu").style.setProperty("--height" , 0);
+};
+
+$(".hamburger").onclick = function(){
+  this.classList.toggle("active")
+  let style = $("#menu").style;
+  if (style.getPropertyValue("--height") === navHeight) {
+    this.classList
+    style.setProperty("--height" , 0);
+  } else {
+    style.setProperty("--height" , navHeight);
   }
-  runMore =  !runMore;
-}
+};
